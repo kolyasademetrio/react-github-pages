3bd61b29a12eede9967d7ffa319fc3f8414949b3
kolyasademetrio <kolyasademetrio@gmail.com>
2020-02-07 14:19:32 +0200

    Updates

---
+++ precache-manifest.8d6f19a621c2222033e807928ad74d76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cabdfb3778ff7ea556665f5d8da8bed3",
+    "url": "/react-github-pages/index.html"
+  },
+  {
+    "revision": "18531bcdcecbaa9fd1eb",
+    "url": "/react-github-pages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0e85f7bd56bf6b0c3e30",
+    "url": "/react-github-pages/static/js/2.2c61db10.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-github-pages/static/js/2.2c61db10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18531bcdcecbaa9fd1eb",
+    "url": "/react-github-pages/static/js/main.c128b57d.chunk.js"
+  },
+  {
+    "revision": "2b1d5ace191403f81c99",
+    "url": "/react-github-pages/static/js/runtime-main.45ac11cf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-github-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "kolyasademetrio.github.io/react-github-pages/precache-manifest.3a227d9c28b2985bb1fcfebccf057058.js"
+  "/react-github-pages/precache-manifest.8d6f19a621c2222033e807928ad74d76.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("kolyasademetrio.github.io/react-github-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-github-pages/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
